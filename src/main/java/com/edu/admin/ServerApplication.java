282d0e4757e3f407fb0c1dfa41cb9a708ad953d2
mengqingan <mengqingan@aerozhonghuan.com>
2019-06-03 15:17:57 +0800

    app

--- src/main/java/com/edu/admin/ServerApplication.java
+++ src/main/java/com/edu/admin/ServerApplication.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 //import org.springframework.jms.annotation.EnableJms;
 
 /**
@@ -23,4 +27,13 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	/**
+	 * 文件上传临时路径
+	 */
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setLocation("/data/temp");
+		return factory.createMultipartConfig();
+	}
 }
