4bc2b00206ad33b79bba5d593306cde3732a2073
wurui <wurui@aerozhonghuan.com>
2019-12-02 14:01:38 +0800

    富文本编辑器

--- src/main/resources/static/js/umeditor/script/umeditor.config.js
+++ src/main/resources/static/js/umeditor/script/umeditor.config.js
@@ -127,6 +127,7 @@
 
     })();
 
+
     /**
      * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
      */
@@ -136,7 +137,7 @@
         UMEDITOR_HOME_URL : URL
 
         //图片上传配置区
-        ,imageUrl:URL+"php/imageUp.php"             //图片上传提交地址
+        ,imageUrl:"http://upload-z2.qiniup.com"             //图片上传提交地址
         ,imagePath:URL + "php/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
         ,imageFieldName:"upfile"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
 
