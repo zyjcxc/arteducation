a7da35fea4c414561570d003b32849c66e3046a7
wurui <wurui@aerozhonghuan.com>
2019-12-03 21:33:27 +0800

    common.js

---
+++ web/js/common.js
@@ -0,0 +1,107 @@
+/**
+ * 文件导入
+ */
+(function (){
+	
+	//新增页面需要配置
+	//code需要与文件名保持一致！
+	var config = {
+		pages:[
+			{
+				code:'teacher',
+				url:'teacher.html',
+				title:'师资团队',
+				isParent:false,
+			},{
+				code:'credentials',
+				url:'credentials.html',
+				title:'证书展示',
+				isParent:false,
+			},
+			{
+				code:'downloads',
+				url:'downloads.html',
+				title:'文件下载',
+				isParent:false,
+			},
+			{
+				code:'videos',
+				url:'videos.html',
+				title:'活动视频',
+				isParent:false,
+			},
+			{
+				code:'news',
+				url:'news.html?news_1',
+				title:'新闻中心',
+				isParent:true,
+				children:[
+				{
+					pCode:'news',
+					code:'news_1',
+					url:'news.html?news_1',
+					title:'新闻'
+				},
+				{
+					pCode:'news',
+					code:'news_2',
+					url:'news.html?news_2',
+					title:'公告'
+				}
+				]
+			}
+		],
+		script:['utils/bootstrap.js']
+	}
+	
+	window.localStorage.setItem('config', JSON.stringify(config));
+	var baseURL = '../';
+	
+	$.ajax({
+	  url: 'http://sy.aerozhonghuan.com/test/qdfaw/api/qingqi/operate/role/list',
+	  type: 'get',
+	  data: { token: '32fb874a1b994fdda902171f16ec1329'},
+	  dataType: 'json',
+	  success: function (res) {
+	  	var data = [
+			{
+				url:'books.html?books_1',
+				title:'新闻'
+			},
+			{
+				url:'books.html?books_2',
+				title:'公告'
+			}
+		];
+	    config.pages.push({
+				url:data[0].url ,
+				title:'新闻中心',
+				isParent:true,
+				children:data});
+	    
+	  }
+	})
+	
+    
+    function initScript(){
+    	for (var i=0,pi;pi = config.script[i++];) {
+        	document.write('<script type="text/javascript" src="'+ baseURL + pi +'"></script>');
+    	}
+    }
+    //页面模板，注意navbar_child.html里有脚本代码
+    function initHtml(){
+    	$('#header-page').load('../pages/common/navbar_child.html');
+		$('#breadcrumb-page').load('../pages/common/breadcrumb_child.html');
+		$('#menu_list').load('../pages/common/menu_child.html');
+		$('#footer-page').load('../pages/common/footer.html');
+    }
+    
+    function init(){
+    	initScript();
+    	initHtml();
+    }
+    
+    init();
+   
+    
+})();
